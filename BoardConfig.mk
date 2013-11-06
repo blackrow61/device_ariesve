27ce0557be5d1e4e9ca31aeabdb9ca7a1e11756b
david <davidteri91@gmail.com>
2013-11-06 20:21:23 +0100

    cortex-a8 variant

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ ARCH_ARM_HAVE_NEON := true
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := scorpion
+TARGET_CPU_VARIANT := cortex-a8
 TARGET_USE_O3 := true
 
 # Dalvik startup with low memory footprint
