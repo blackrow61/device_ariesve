7470971399322d0d41dd9e6a22646d7b676c0886
david <davidteri91@gmail.com>
2013-11-13 14:42:25 +0100

    add loudness enhancer audio effect audio: add device specific audio_effects.conf file Audio preprocessors are no longer included in the default audio_effects.conf as of 4.2, so add an audio_effects.conf exposing the effects that are supported by ariesve.

--- ariesve.mk
+++ ariesve.mk
@@ -42,7 +42,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/config/audio_effects.conf:system/vendor/etc/audio_policy.conf
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
