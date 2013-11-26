fb934a59989025dd16563d66fd102f04b6a30245
david <davidteri91@gmail.com>
2013-11-26 01:15:06 +0100

    Update boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,26 +213,26 @@ BOARD_SEPOLICY_DIRS += \
     device/samsung/ariesve/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    app.te \
-    bluetooth.te \
+    file_contexts \
+    property_contexts \
+    bridge.te \
+    camera.te \
     device.te \
+    dhcp.te \
     domain.te \
     drmserver.te \
-    file_contexts \
-    files \
     file.te \
-    hci_init.te \
-    healthd.te \
+    geomagneticd.te \
     init.te \
-    init_shell.te \
-    keystore.te \
-    kickstart.te \
+    mac_update.te \
     mediaserver.te \
     rild.te \
+    rmt.te \
     surfaceflinger.te \
     system.te \
+    tee.te \
     ueventd.te \
-    untrusted_app.te \
-    vold.te \
-    wpa.te \
-    wpa_socket.te
+    wpa_supplicant.te
+
+# Support old recoveries for 4.4
+USE_SET_METADATA := false
